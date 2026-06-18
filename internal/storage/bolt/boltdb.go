be70eb7716fae1a1dcf9768360cd7771727f5d55
pouyasadri <pouyapaghandeh78@gmail.com>
2026-06-18 14:52:29 +0200

    Implement BoltDB storage for blockchain data management

---
+++ internal/storage/bolt/boltdb.go
@@ -0,0 +1,155 @@
+package bolt
+
+import (
+	"fmt"
+
+	"github.com/boltdb/bolt"
+	"github.com/pouyasadri/go-blockchain/internal/storage"
+)
+
+const (
+	blocksBucket = "blocks"
+	utxoBucket   = "chainstate"
+	tipKey       = "l"
+)
+
+// DB represents a BoltDB storage implementation
+type DB struct {
+	db *bolt.DB
+}
+
+// Open creates a new BoltDB connection
+func Open(dbFile string) (*DB, error) {
+	db, err := bolt.Open(dbFile, 0600, nil)
+	if err != nil {
+		return nil, fmt.Errorf("failed to open bolt db: %w", err)
+	}
+
+	// Ensure buckets exist
+	err = db.Update(func(tx *bolt.Tx) error {
+		_, err := tx.CreateBucketIfNotExists([]byte(blocksBucket))
+		if err != nil {
+			return err
+		}
+
+		_, err = tx.CreateBucketIfNotExists([]byte(utxoBucket))
+		return err
+	})
+
+	if err != nil {
+		return nil, fmt.Errorf("failed to initialize buckets: %w", err)
+	}
+
+	return &DB{db: db}, nil
+}
+
+// Close closes the bolt database
+func (d *DB) Close() error {
+	return d.db.Close()
+}
+
+func (d *DB) SaveBlock(hash []byte, blockData []byte) error {
+	return d.db.Update(func(tx *bolt.Tx) error {
+		b := tx.Bucket([]byte(blocksBucket))
+		return b.Put(hash, blockData)
+	})
+}
+
+func (d *DB) GetBlock(hash []byte) ([]byte, error) {
+	var blockData []byte
+	err := d.db.View(func(tx *bolt.Tx) error {
+		b := tx.Bucket([]byte(blocksBucket))
+		data := b.Get(hash)
+		if data == nil {
+			return storage.ErrNotFound
+		}
+		// bolt DB returns a byte slice that is only valid for the life of the transaction.
+		// we must copy it.
+		blockData = make([]byte, len(data))
+		copy(blockData, data)
+		return nil
+	})
+	return blockData, err
+}
+
+func (d *DB) GetTip() ([]byte, error) {
+	var tip []byte
+	err := d.db.View(func(tx *bolt.Tx) error {
+		b := tx.Bucket([]byte(blocksBucket))
+		data := b.Get([]byte(tipKey))
+		if data == nil {
+			return storage.ErrNotFound
+		}
+		tip = make([]byte, len(data))
+		copy(tip, data)
+		return nil
+	})
+	return tip, err
+}
+
+func (d *DB) UpdateTip(hash []byte) error {
+	return d.db.Update(func(tx *bolt.Tx) error {
+		b := tx.Bucket([]byte(blocksBucket))
+		return b.Put([]byte(tipKey), hash)
+	})
+}
+
+func (d *DB) GetUTXO(txID []byte) ([]byte, error) {
+	var outsData []byte
+	err := d.db.View(func(tx *bolt.Tx) error {
+		b := tx.Bucket([]byte(utxoBucket))
+		data := b.Get(txID)
+		if data == nil {
+			return storage.ErrNotFound
+		}
+		outsData = make([]byte, len(data))
+		copy(outsData, data)
+		return nil
+	})
+	return outsData, err
+}
+
+func (d *DB) SaveUTXO(txID []byte, outsData []byte) error {
+	return d.db.Update(func(tx *bolt.Tx) error {
+		b := tx.Bucket([]byte(utxoBucket))
+		return b.Put(txID, outsData)
+	})
+}
+
+func (d *DB) DeleteUTXO(txID []byte) error {
+	return d.db.Update(func(tx *bolt.Tx) error {
+		b := tx.Bucket([]byte(utxoBucket))
+		return b.Delete(txID)
+	})
+}
+
+func (d *DB) ClearUTXOSet() error {
+	return d.db.Update(func(tx *bolt.Tx) error {
+		err := tx.DeleteBucket([]byte(utxoBucket))
+		if err != nil && err != bolt.ErrBucketNotFound {
+			return err
+		}
+		_, err = tx.CreateBucket([]byte(utxoBucket))
+		return err
+	})
+}
+
+func (d *DB) IterateUTXO(yield func(k, v []byte) bool) error {
+	return d.db.View(func(tx *bolt.Tx) error {
+		b := tx.Bucket([]byte(utxoBucket))
+		c := b.Cursor()
+		for k, v := c.First(); k != nil; k, v = c.Next() {
+			// Bolt DB slices are only valid for the transaction, but we yield them synchronously
+			// We can yield them directly if the caller doesn't keep them across iterations,
+			// or we can copy. Copying is safer.
+			kc := make([]byte, len(k))
+			copy(kc, k)
+			vc := make([]byte, len(v))
+			copy(vc, v)
+			if !yield(kc, vc) {
+				break
+			}
+		}
+		return nil
+	})
+}
---
+++ internal/storage/bolt/boltdb_test.go
@@ -0,0 +1,74 @@
+package bolt
+
+import (
+	"path/filepath"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestBoltDBStorage(t *testing.T) {
+	dir := t.TempDir()
+	dbFile := filepath.Join(dir, "test.db")
+
+	db, err := Open(dbFile)
+	assert.NoError(t, err)
+	assert.NotNil(t, db)
+	defer db.Close()
+
+	// Test GetTip initially
+	tip, err := db.GetTip()
+	assert.Error(t, err) // bucket shouldn't exist or no 'l' key
+	assert.Empty(t, tip)
+
+	// Test UpdateTip
+	hash := []byte("lasthash")
+	err = db.UpdateTip(hash)
+	assert.NoError(t, err)
+
+	// Test SaveBlock
+	err = db.SaveBlock([]byte("blockhash"), []byte("blockdata"))
+	assert.NoError(t, err)
+
+	// Test GetBlock
+	data, err := db.GetBlock([]byte("blockhash"))
+	assert.NoError(t, err)
+	assert.Equal(t, []byte("blockdata"), data)
+
+	// Test SaveUTXO and IterateUTXO
+	err = db.SaveUTXO([]byte("txid"), []byte("txouts"))
+	assert.NoError(t, err)
+
+	err = db.IterateUTXO(func(k, v []byte) bool {
+		assert.Equal(t, []byte("txid"), k)
+		assert.Equal(t, []byte("txouts"), v)
+		return true
+	})
+	assert.NoError(t, err)
+
+	// Test DeleteUTXO
+	err = db.DeleteUTXO([]byte("txid"))
+	assert.NoError(t, err)
+
+	// Test Iterate again to ensure deletion
+	count := 0
+	err = db.IterateUTXO(func(k, v []byte) bool {
+		count++
+		return true
+	})
+	assert.NoError(t, err)
+	assert.Equal(t, 0, count)
+
+	// Test ClearUTXOSet
+	err = db.SaveUTXO([]byte("txid2"), []byte("txouts2"))
+	assert.NoError(t, err)
+	err = db.ClearUTXOSet()
+	assert.NoError(t, err)
+	count = 0
+	err = db.IterateUTXO(func(k, v []byte) bool {
+		count++
+		return true
+	})
+	assert.NoError(t, err)
+	assert.Equal(t, 0, count)
+}
